1694046408b8594f9cf48e0690b4604f57641504
jainhitesh3 <31804597+jainhitesh3@users.noreply.github.com>
2019-08-23 11:22:34 +0530

    Create updateHTML.rb
    This file updates the existing report with the correct test count and adds a pie chart with that.

---
+++ updateHTML.rb
@@ -0,0 +1,75 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+$total_count = 0
+$pass_count = 0
+$fail_count = 0
+
+def calculateTestCaseCount()
+  file = File.read('/Users/hiteshjain/Downloads/report_final_1.html')
+  doc = Nokogiri::HTML(file)
+  testCaseNames = []
+  doc.xpath('//h3[@class=\'title\']').each do |link|
+    value = link.content
+    if !(value.include? "TestSuiteName")
+      if !(testCaseNames.include? value)
+        testCaseNames.push(value)
+        $total_count = $total_count + 1
+      end
+    end
+  end
+  doc.xpath('//h3[@class=\'time\']').each do |link|
+    $pass_count = $pass_count + 1
+  end
+  $fail_count = $total_count - $pass_count
+end
+
+def showUpdatedTestCount()
+  doc = File.open("/Users/hiteshjain/Downloads/report_final_1.html") { |f| Nokogiri::HTML(f) }
+  h1 = doc.at_xpath "//*[@id=\"test-count\"]/span"
+  h1.content = $total_count
+  h1 = doc.at_xpath "//*[@id=\"fail-count\"]/span"
+  h1.content = $fail_count
+  File.write("/Users/hiteshjain/Downloads/report_final_1.html",doc.to_html)
+end
+
+def addPieChart()
+  pieChart = "<section class=\"piechart\">
+        <div id=\"piechart\" align=\"middle\" style=\"vertical-align: top;\"></div>
+        <script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>
+
+        <script type=\"text/javascript\">
+        // Load google charts
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        // Draw the chart and set the chart values
+        function drawChart() {
+          var data = google.visualization.arrayToDataTable([
+          ['Result', 'Count'],
+          ['Pass', $PASS_COUNT],
+          ['Fail', $FAIL_COUNT]
+        ]);
+
+          // Optional; add a title and set the width and height of the chart
+          var options = {'title':'Test Results', 'width':450, 'height':300 };
+
+          // Display the chart inside the <div> element with id=\"piechart\"
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+          chart.draw(data, options);
+        }
+        </script>
+        </section>"
+
+  pieChart["$PASS_COUNT"] = $pass_count.to_s
+  pieChart["$FAIL_COUNT"] = $fail_count.to_s
+  doc = File.open("/Users/hiteshjain/Downloads/report_final_1.html") { |f| Nokogiri::HTML(f) }
+  h1 = doc.at_xpath "/html/body/header"
+  h1.add_next_sibling pieChart
+  File.write("/Users/hiteshjain/Downloads/report_final_1.html",doc.to_html)
+end
+
+calculateTestCaseCount()
+showUpdatedTestCount()
+addPieChart()
